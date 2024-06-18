e5c856f8172187dbaf39295a028825af0669f55b
unknown <c1420263@st.kanazawa-it.ac.jp>
2024-06-18 18:45:11 +0900

    farst

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default function taichi() {
+  return (
+    <div>
+      <h1>自己紹介</h1>
+      <p>名前：和田　泰知</p>
+      <p>所属：工学部　情報工学科</p>
+      <p>出身地：富山県富山市</p>
+      <p>好きな食べ物：卵かけご飯</p>
+ <img src="/ramen.png" alt="ラーメンの画像" />
+    </div >  
+  )
+}
